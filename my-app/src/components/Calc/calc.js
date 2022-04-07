07f26359779b19cd100a118e594ad857b37ac47d
Dobromiła Okrasa <dobromila.okrasa@unit9.com>
2022-04-07 09:40:04 +0200

    Refactor styles

--- my-app/src/components/Calc/calc.js
+++ my-app/src/components/Calc/calc.js
@@ -11,33 +11,33 @@ function Calc() {
       <div id="field">0</div>
       <div id="container">
         <div className="row">
-          <button className="darkgrey">AC</button>
-          <button className="darkgrey">+/-</button>
-          <button className="darkgrey">%</button>
-          <button className="orange">&#247;</button>
+          <button>AC</button>
+          <button>+/-</button>
+          <button>%</button>
+          <button>&#247;</button>
         </div>
         <div className="row">
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button className="orange">	&#215;</button>
+          <button>	&#215;</button>
         </div>
         <div className="row">
           <button>4</button>
           <button>5</button>
           <button>6</button>
-          <button className="orange">&#8722;</button>
+          <button>&#8722;</button>
         </div>
         <div className="row">
           <button>1</button>
           <button>2</button>
           <button>3</button>
-          <button className="orange">+</button>
+          <button>+</button>
         </div>
         <div className="row">
           <button id="big">0</button>
           <button>,</button>
-          <button className="orange">=</button>
+          <button>=</button>
         </div>
       </div>
     </div>

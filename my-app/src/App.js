c445517b832050a355c45f889f74f494406e5557
Dobromiła Okrasa <dobromila.okrasa@unit9.com>
2022-04-07 09:34:29 +0200

    Extract Calc component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,48 +1,12 @@
+import Calc from './components/Calc/calc';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div id="main">
-          <div id="menu">
-            <span>&#215;</span>
-            <span>&#8722;</span>
-            <span>+</span>
-          </div>
-          <div id="field">0</div>
-          <div id="container">
-            <div className="row">
-              <button className="darkgrey">AC</button>
-              <button className="darkgrey">+/-</button>
-              <button className="darkgrey">%</button>
-              <button className="orange">&#247;</button>
-            </div>
-            <div className="row">
-              <button>7</button>
-              <button>8</button>
-              <button>9</button>
-              <button className="orange">	&#215;</button>
-            </div>
-            <div className="row">
-              <button>4</button>
-              <button>5</button>
-              <button>6</button>
-              <button className="orange">&#8722;</button>
-            </div>
-            <div className="row">
-              <button>1</button>
-              <button>2</button>
-              <button>3</button>
-              <button className="orange">+</button>
-            </div>
-            <div className="row">
-              <button id="big">0</button>
-              <button>,</button>
-              <button className="orange">=</button>
-            </div>
-          </div>
-        </div>
+        <Calc />
       </header>
     </div>
   );
